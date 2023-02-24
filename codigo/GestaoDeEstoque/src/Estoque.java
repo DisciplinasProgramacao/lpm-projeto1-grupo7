1fb589e2e411e13bc909f1be005aec703a592e5c
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-02-23 21:45:27 -0300

    criando construtor, getters e setters de cada atributo

--- codigo/GestaoDeEstoque/src/Estoque.java
+++ codigo/GestaoDeEstoque/src/Estoque.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
 
 public class Estoque {
-    ArrayList<Produto> produtos = new ArrayList<Produto>();
+    ArrayList<Produto> produtos = new ArrayList<>();
 }
--- codigo/GestaoDeEstoque/src/Produto.java
+++ codigo/GestaoDeEstoque/src/Produto.java
@@ -1,6 +1,94 @@
 public class Produto {
+    private int id;
     private String nome, descricao;
-    private int id, quantidadeMinima;
-    private double precoCusto, precoVenda, margemLucro, somaVendas, imposto, quantidadeProduto;
+    private double precoDeCusto, precoDeVenda;
+    private double margemDeLucro, valorDoImposto;
+    private static double valorArrecadado;
+    private static int quantidade;
+    private int quantidadeMinimaNecessaria;
 
+    // CLASS CONSTRUCTOR
+    public Produto() {
+    }
+
+    // GETTERS AND SETTERS
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPrecoDeCusto() {
+        return precoDeCusto;
+    }
+
+    public void setPrecoDeCusto(double precoDeCusto) {
+        this.precoDeCusto = precoDeCusto;
+    }
+
+    public double getPrecoDeVenda() {
+        return precoDeVenda;
+    }
+
+    public void setPrecoDeVenda(double precoDeVenda) {
+        this.precoDeVenda = precoDeVenda;
+    }
+
+    public double getMargemDeLucro() {
+        return margemDeLucro;
+    }
+
+    public void setMargemDeLucro(double margemDeLucro) {
+        this.margemDeLucro = margemDeLucro;
+    }
+
+    public double getValorDoImposto() {
+        return valorDoImposto;
+    }
+
+    public void setValorDoImposto(double valorDoImposto) {
+        this.valorDoImposto = valorDoImposto;
+    }
+
+    public static double getValorArrecadado() {
+        return valorArrecadado;
+    }
+
+    public static void setValorArrecadado(double valorArrecadado) {
+        Produto.valorArrecadado = valorArrecadado;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getQuantidadeMinimaNecessaria() {
+        return quantidadeMinimaNecessaria;
+    }
+
+    public void setQuantidadeMinimaNecessaria(int quantidadeMinimaNecessaria) {
+        this.quantidadeMinimaNecessaria = quantidadeMinimaNecessaria;
+    }
 }
