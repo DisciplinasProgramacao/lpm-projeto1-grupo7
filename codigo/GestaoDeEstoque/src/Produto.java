a4a41b7f3ae57ede205c50f220d1781358ff48de
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-02-23 21:13:39 -0300

    criando alguns atributos de classes

--- codigo/GestaoDeEstoque/src/Produto.java
+++ codigo/GestaoDeEstoque/src/Produto.java
@@ -1,3 +1,6 @@
 public class Produto {
-  
+    private String nome, descricao;
+    private int id, quantidadeMinima;
+    private double precoCusto, precoVenda, margemLucro, somaVendas, imposto, quantidadeProduto;
+
 }
