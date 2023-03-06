dc6d56a459e6c857caae25224b063e5d029fe6b8
Samuel Leal <samuelmarquessousaleal@gmail.com>
2023-03-06 09:31:34 -0300

    Começando o Main #6

--- codigo/GestaoDeEstoque/src/Main.java
+++ codigo/GestaoDeEstoque/src/Main.java
@@ -1,5 +1,23 @@
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        Produto produto = new Produto("Descricao", 10, 10 );
+        Produto feijao = new Produto();
+        Produto arroz = new Produto();
 
+        Estoque estoque = new Estoque(100);
+
+        produto.registraMargemDeLucro(40);
+        produto.comprarProduto(10);
+        produto.venderProduto(3);
+        produto.print();
+
+        estoque.Guardar(produto);
+        estoque.Guardar(arroz);
+        estoque.Guardar(feijao);
+        estoque.Remover("arroz");
+        System.out.println(estoque.procurar("feijao"));
+        estoque.ProdutosAbaixoEstoqueMinimo();
+        
     }
 }
