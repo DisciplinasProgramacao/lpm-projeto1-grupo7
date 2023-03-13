5ae3acaed07a4511e2e506ce9836692f75059457
Samuel Leal <samuelmarquessousaleal@gmail.com>
2023-03-12 23:11:07 -0300

    Atualizando Main #6

--- codigo/GestaoDeEstoque/src/Main.java
+++ codigo/GestaoDeEstoque/src/Main.java
@@ -1,23 +1,30 @@
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Produto produto = new Produto("Descricao", 10, 10 );
-        Produto feijao = new Produto("feijao", 6, 50);
-        Produto arroz = new Produto("arroz", 12, 40);
+        Produto arroz = new Produto("arroz", 10, 40, 20);
+        Produto feijao = new Produto("feijao", 7, 45, 18);
+        Produto macarrao = new Produto("macarrao", 6, 470, 20);
+        Produto morango = new Produto("morango", 2, 80, 40);
+        Produto chocolate = new Produto("chocolate", 8, 50, 5);
+        Estoque estoque = new Estoque(10);
 
-        Estoque estoque = new Estoque(100);
+        morango.comprarProduto(43);
+        arroz.comprarProduto(30);
+        chocolate.venderProduto(4);
 
-        produto.registraMargemDeLucro(40);
-        produto.comprarProduto(10);
-        produto.venderProduto(3);
-        produto.print();
-
-        estoque.Guardar(produto);
+        estoque.Guardar(macarrao);
         estoque.Guardar(arroz);
         estoque.Guardar(feijao);
+        estoque.Guardar(morango);
+        estoque.Guardar(chocolate);
+
         estoque.Remover("arroz");
-        System.out.println(estoque.procurar("feijao"));
-        estoque.ProdutosAbaixoEstoqueMinimo();
+
+        System.out.println("Informaçöes do produto" + estoque.procurar("feijao").print());
+
+        System.out.println("Produto(s) abaixo do estoque:" + estoque.ProdutosAbaixoEstoqueMinimo().print());
+
+        System.out.println("O estoque vale:" + estoque.valorTotalEmEstoque());
         
     }
 }
